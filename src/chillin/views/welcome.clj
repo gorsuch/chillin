155d35aeaf51b8ba62037a84fa81c18d4c547766
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-03 14:26:32 -0500

    first commit

---
+++ src/chillin/views/welcome.clj
@@ -0,0 +1,10 @@
+(ns chillin.views.welcome
+  (:require [chillin.views.common :as common]
+            [noir.content.pages :as pages])
+  (:use noir.core
+        hiccup.core
+        hiccup.page-helpers))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to chillin"]))
