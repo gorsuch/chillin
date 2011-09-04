30959df30ce913d75e349ccf416e8936df5621d3
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-03 22:41:48 -0500

    require clj-couchdb

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject chillin "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.1.0"]]
+                           [noir "1.1.0"]
+                           [clj-couchdb "0.0.1-SNAPSHOT"]]
             :main chillin.server)
 
