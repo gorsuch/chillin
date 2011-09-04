899754f3b16363fdad4c46ea120a612e49b5135a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-09-03 22:42:21 -0500

    remove the default noir pages

--- src/chillin/views/welcome.clj
+++ src/chillin/views/welcome.clj
@@ -1,10 +1,9 @@
 (ns chillin.views.welcome
-  (:require [chillin.views.common :as common]
-            [noir.content.pages :as pages])
+  (:require [chillin.views.common :as common])
   (:use noir.core
         hiccup.core
         hiccup.page-helpers))
 
-(defpage "/welcome" []
+(defpage "/" []
          (common/layout
            [:p "Welcome to chillin"]))
